23c733902eb4c6f781c1c83229e6a99c35bc35cf
Karmesh jain <61316837+Karmeshjain@users.noreply.github.com>
2021-06-08 09:35:40 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.activitylifecycleandinstancestate;
+package com.example.explicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,36 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("lifecycle","onCreate invoked");
     }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("lifecycle","onStart invoked");
+    public void callSecondActivity(View view){
+        Intent i = new Intent(getApplicationContext(), SecondactivityKarmesh.class);
+        i.putExtra("Value1", "Karmesh(500075888)");
+        i.putExtra("Value2", "R142219014");
+        // Set the request code to any code you like, you can identify the
+        // callback via this code
+        startActivity(i);
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("lifecycle","onResume invoked");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("lifecycle","onPause invoked");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("lifecycle","onStop invoked");
-    }
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("lifecycle","onRestart invoked");
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("lifecycle","onDestroy invoked");
-    }
-    }
+}
